0209d105af5244e10b0486683534ee6173840bdd
Ivan Kucherenko <i.kucherenko@1er.app>
2025-03-24 20:22:18 +0500

    some changes

--- core/src/main.c
+++ core/src/main.c
@@ -5,101 +5,34 @@
 #include <unistd.h>
 #include <unistd.h> // read(), write(), close()
 
-#include "./io/io.h"
-#include "./utils/utils.h"
+const unsigned int STDIN_READ_BUFFER_SIZE = 128;
+const unsigned int CMD_WORD_SIZE = 32;
+const char QUIT_CMD_SEQ[] = {"__CMD@>QUIT"};
 
-void setup_files() {
-  setup_lockfile();
-  setup_indexfile();
-  setup_datafile();
-}
+int cmd_cmp(const char *a, const char *b) {
+  int l_a = strnlen(a, CMD_WORD_SIZE);
+  int l_b = strnlen(b, CMD_WORD_SIZE);
+  int len = l_a < l_b ? l_a : l_b;
 
-// CRUD Operations
-void Create(const Blob *record) {
-  int head_pos = write_new_record(record);
-  write_index(head_pos, -1);
-}
-
-Blob *Read(int id) {
-  int record_pos = get_record_pos_by_id(id);
-
-  Blob *result = read_record(record_pos);
-
-  return result;
-}
-
-// functions below require solution for fragmentation problem to work
-// efficiently another index for free space and more complex allocation logic
-// will be implemented in v0.2
-void Update() { /* think of it later */ }
-void Delete() { /* just set the flags */ }
-void HardDelete() { /* write zeros over record OR clear index to make it
-                       unsearchable OR both */
-}
-
-int main() {
-  setup_files();
-
-  // unsigned char test_payload[] =
-  //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris nec "
-  //     "tincidunt neque. Duis nulla lectus, tristique ac mattis quis, "
-  //     "efficitur "
-  //     "ac nunc. Integer mollis, dolor at dictum vulputate, arcu ipsum "
-  //     "commodo "
-  //     "neque, nec imperdiet diam dui feugiat libero. Donec eget tempus ante,
-  //     " "id posuere turpis. Fusce molestie nisi tincidunt augue.";
-  // int payload_size = sizeof(test_payload);
-  //
-  // for (int i = 0; i < 100; i++)
-  //   Create(test_payload, payload_size);
-
-  unsigned char words[][30] = {"one", "two", "three", "four", "five",
-                               "f",   "u",   "gay",   "meow", "test"};
-  int l = 10;
-
-  printf("start\n");
-
-  // for (int k = 0; k < 10000; k++) {
-  //   for (int i = 0; i < l; i++) {
-  //     int size = 0;
-  //     unsigned char *t = Read(2 + i * k, &size);
-  //
-  //     printf("%s\n", t);
-  //   }
-  // }
-
-  printf("end\n");
-
-  for (int i = 0; i < 9; i++) {
-    Blob *b = malloc(sizeof(Blob));
-
-    b->size = 30;
-    b->data = malloc(30);
-
-    memcpy(b->data, words[i], 30);
-
-    printf("Word: %s\n", words[i]);
-
-    Create(b);
-
-    free(b->data);
-    free(b);
+  for (int i = 0; i < len; i++) {
+    if (a[i] != b[i]) {
+      return 0;
+    }
   }
-  //
-  // int size = -1;
-  // unsigned char *data = Read(8, &size);
-  // // Read(919);
-  //
-  // unsigned char str[size];
-  // memcpy(str, data, size);
-  //
-  // printf("%s\n", str);
+  return 1;
+}
 
-  // FILE *test;
-  // test = fopen("test.bin", "r");
-  // fseek(test, -16, SEEK_END);
-  // char line[100];
-  //
-  // fgets(line, sizeof(line), test);
-  // printf("%s", line);
+int main(int argc, char **arv) {
+  unsigned char stdin_read_buffer[STDIN_READ_BUFFER_SIZE];
+  while (true) {
+    fread(stdin_read_buffer, STDIN_READ_BUFFER_SIZE, 1, stdin);
+    // printf("after%s\n\n", stdin_read_buffer);
+    fwrite(stdin_read_buffer, STDIN_READ_BUFFER_SIZE, 1, stdout);
+
+    int res = cmd_cmp(QUIT_CMD_SEQ, (char *)stdin_read_buffer);
+    printf("\n\n[DEBUG]:\tres %d\n\n", res);
+    if (res) {
+      return 123;
+    }
+  }
 }
